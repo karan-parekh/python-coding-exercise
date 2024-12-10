1e85b72bf749f236cd8ed7f69232e1c97d120c1f
Karan Parekh <karanparekh501@gmail.com>
2024-12-10 16:43:04 +0530

    splitter initial version

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -3,12 +3,79 @@ from coding_exercise.domain.model.cable import Cable
 
 class Splitter:
 
-    def __validate(self):
+    def __init__(self):
+        self.longest_int = None
+        self.result = []
+        self.cable_count = 0
+        self.last_count = 0
+
+    def __validate(self, cable: Cable, times: int):
         valid = True
+        message = "Invalid inputs"
+        length = cable.length
+
+        if times >= length:
+            valid = False
+            message = f"Number of splits cannot be greater than or equal to the length of cable | {times} >= {length}"
+
+        if length < 2 or cable.length > 1024:
+            valid = False
+            message = f"Length of cable must be between 2 and 1024 | length = {length}"
+
+        if times < 1 or times > 64:
+            valid = False
+            message = f"Cannot split less than 1 times or more than 64 times | times = {times}"
+
         if not valid:
-            raise ValueError
+            raise ValueError(message)
 
     def split(self, cable: Cable, times: int) -> list[Cable]:
-        self.__validate()
+        self.__validate(cable, times)
+        name = cable.name
+        length = cable.length
+        parts = times + 1
 
-        return []
+        print(f"For a cable of length {length}, split it {times} times. This means that there will be {parts} parts")
+
+        if not self.longest_int:
+            self.longest_int = length // parts
+
+        print(f"Longest integer length is {self.longest_int}")
+        
+        if self.longest_int == 1:
+            self.cable_count = length
+            padding = len(str(self.cable_count))
+            result = []
+            for c in range(length):
+                tail = f"{c}".zfill(padding)
+                result.append(Cable(1, f"{name}-{tail}"))
+
+            return result
+        
+        remaining_len = length - (self.longest_int * times)
+                       
+        if not self.cable_count:
+            self.cable_count = parts+1 if remaining_len > self.longest_int else parts
+
+        padding = len(str(self.cable_count))
+
+        print(f"appending cable of len {self.longest_int} to the result array, {times} times")
+        for c in range(times):
+            tail = f"{self.last_count}".zfill(padding)
+            self.result.append(Cable(self.longest_int, f"{name}-{tail}"))
+            self.last_count += 1
+
+        if remaining_len > self.longest_int:
+            print(f"Since remaining len {remaining_len} > longest int {self.longest_int}")
+            remaining_times = remaining_len // self.longest_int
+            print(f"Split it again {remaining_times} times")
+            self.split(Cable(remaining_len, name), remaining_times)
+        elif remaining_len:
+            print(f"Since remaining len {remaining_len} <= longest int {self.longest_int}")
+            print(f"We append cable of remaining len {remaining_len} to the result array")
+            tail = f"{self.last_count}".zfill(padding)
+            self.result.append(Cable(remaining_len, f"{name}-{tail}"))
+            self.last_count += 1
+
+        print(f"Total cables in array: {len(self.result)}")
+        return self.result
